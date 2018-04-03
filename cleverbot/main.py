7a864fc693062b193462aed894fb8e4c1da6c7d3
Jordan Dalcq <jordan@iMac-de-Jordan.local>
2018-04-03 17:51:12 +0200

    UPDATE

--- cleverbot/main.py
+++ cleverbot/main.py
@@ -24,6 +24,5 @@ class Cleverbot(object):
 
     def get(self):
         return self.driver.execute_script("return cleverbot.reply")
-
     def send(self, reply):
-        return self.driver.execute_script("cleverbot.sendAI('%s')" % (reply))
+        return self.driver.execute_script("cleverbot.sendAI('%s')" % (reply.encode("utf-8").replace('\'', ' ').replace("à", "a").replace("ï", "i").replace("\"", " ").replace("@", "").replace("ô", "o").replace("ê", "e").replace("é", "e").replace("è", "e").lower()))
